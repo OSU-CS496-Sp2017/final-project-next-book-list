82a659955dce5cfc52bb74a7072492dd1baa06ec
Evan Milton <jimdandy@10-248-14-57.wireless.oregonstate.edu>
2017-06-14 10:42:00 -0700

    refactored parser

--- MyBookList/app/src/main/java/jimdandy/mybooklist/Utilities/GoodReadsUtils.java
+++ MyBookList/app/src/main/java/jimdandy/mybooklist/Utilities/GoodReadsUtils.java
@@ -69,47 +69,23 @@ public class GoodReadsUtils {
 
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//            factory.setIgnoringComments(true);
-//            factory.setIgnoringElementContentWhitespace(true);
-//            factory.setValidating(true);
             DocumentBuilder builder = factory.newDocumentBuilder();
 
             InputStream searchResultsStream = new ByteArrayInputStream(searchResultsXML.getBytes("UTF-8"));
             Document xml = builder.parse(searchResultsStream);
 
             NodeList worksList = xml.getElementsByTagName("work");
-            Log.d("PARSER: ", "NUMBER OF WORKS: " + worksList.getLength());
-
             for (int i = 0; i < worksList.getLength(); i++) {
+                Element work = (Element)worksList.item(i);
+                NodeList bestBookList = work.getElementsByTagName("best_book");
+                Element bestBook = (Element)bestBookList.item(0);
 
                 SearchResult searchResult = new SearchResult();
-                Element workElement = (Element)worksList.item(i);
 
                 //      PUBLICATION DATE
-                NodeList pubDayList = workElement.getElementsByTagName("original_publication_month");
-                NodeList pubMonthList = workElement.getElementsByTagName("original_publication_month");
-                NodeList pubYearList = workElement.getElementsByTagName("original_publication_year");
-
-                Element pubDayElement = (Element)pubDayList.item(0);
-                Node pubDayNode = pubDayElement.getChildNodes().item(0);
-                String pubDay = "";
-                if (pubDayNode != null) {
-                    pubDay = pubDayNode.getNodeValue();
-                }
-
-                Element pubMonthElement = (Element)pubMonthList.item(0);
-                Node pubMonthNode = pubMonthElement.getChildNodes().item(0);
-                String pubMonth = "";
-                if (pubMonthNode != null) {
-                    pubMonth = pubMonthNode.getNodeValue();
-                }
-
-                Element pubYearElement = (Element)pubYearList.item(0);
-                Node pubYearNode = pubYearElement.getChildNodes().item(0);
-                String pubYear = "";
-                if (pubMonthNode != null) {
-                    pubYear = pubYearNode.getNodeValue();
-                }
+                String pubDay = getXMLTagValue(work, "original_publication_day");
+                String pubMonth = getXMLTagValue(work, "original_publication_month");
+                String pubYear = getXMLTagValue(work, "original_publication_year");
 
                 String fullPubDate = "";
                 if (pubDay != "") {
@@ -124,90 +100,36 @@ public class GoodReadsUtils {
 
                 searchResult.publicationDate = fullPubDate;
 
-
                 //      AVERAGE RATING
-                NodeList avgRatingList = workElement.getElementsByTagName("average_rating");
-
-                Element avgRatingElement = (Element)avgRatingList.item(0);
-                Node avgRatingNode = avgRatingElement.getChildNodes().item(0);
-                String avgRatingValue = "unrated";
-                if (avgRatingNode != null) {
-                    avgRatingValue = String.valueOf(avgRatingNode.getNodeValue());
-                }
-
+                String avgRatingValue = getXMLTagValue(work, "average_rating");
                 searchResult.avgRating = avgRatingValue;
 
-
-                //*** These are used for all of the remaining values
-                NodeList bestBookList = workElement.getElementsByTagName("best_book");
-                Element bestBook = (Element)bestBookList.item(0);
-                ////*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***
-
-
                 //      TITLE
-                NodeList titleList = bestBook.getElementsByTagName("title");
-                Element titleElement = (Element)titleList.item(0);
-                Node titleNode = titleElement.getChildNodes().item(0);
-                String titleValue = "no title";
-                if (titleNode != null) {
-                    titleValue = titleNode.getNodeValue();
-                }
-
+                String titleValue = getXMLTagValue(bestBook, "title");
                 searchResult.title = titleValue;
 
-
                 //      AUTHOR
-                NodeList nameList = bestBook.getElementsByTagName("name");
-                Element nameElement = (Element)nameList.item(0);
-                Node nameNode = nameElement.getChildNodes().item(0);
-                String authorName = "unknown author";
-                if (nameNode != null) {
-                    authorName = nameNode.getNodeValue();
-                }
-
-                searchResult.author = authorName;
+                String authorValue = getXMLTagValue(bestBook, "name");
+                searchResult.author = authorValue;
 
                 //      URLS
-                NodeList URLList = bestBook.getElementsByTagName("image_url");
-                Element URLElement = (Element)URLList.item(0);
-                Node URLNode = URLElement.getChildNodes().item(0);
-                String url = "";
-                if (URLNode != null) {
-                    url = URLNode.getNodeValue();
-                }
+                String largeURLValue = getXMLTagValue(bestBook, "image_url");
+                searchResult.largeImageURL = largeURLValue;
 
-                searchResult.largeImageURL = url;
-
-
-                NodeList smallURLList = bestBook.getElementsByTagName("small_image_url");
-                Element smallURLElement = (Element)smallURLList.item(0);
-                Node smallURLNode = smallURLElement.getChildNodes().item(0);
-                url = "";
-                if (smallURLNode != null) {
-                    url = smallURLNode.getNodeValue();
-                }
-
-                searchResult.smallImageURL = url;
-
+                String smallURLValue = getXMLTagValue(bestBook, "small_image_url");
+                searchResult.smallImageURL = smallURLValue;
 
                 //      ID
-                NodeList IDList = bestBook.getElementsByTagName("id");
-                Element IDElement = (Element)IDList.item(0);
-                Node IDNode = IDElement.getChildNodes().item(0);
-                String IDValue = "";
-                if (IDNode != null) {
-                    IDValue = IDNode.getNodeValue();
-                }
-
+                String IDValue = getXMLTagValue(bestBook, "id");
                 searchResult.goodReadsBeskBookID = IDValue;
 
-
-                // BUILD RETURN LIST
+                // ADD TO RETURN LIST
                 Log.d("UTILS: ", searchResult.title + " by " + searchResult.author +
                         " published: " + searchResult.publicationDate +
                         " with an average rating of " + searchResult.avgRating + "\n" +
                         "URLs at: " + searchResult.largeImageURL + " and " + searchResult.smallImageURL
                 );
+
                 formattedResultsList.add(searchResult);
             }
         } catch (Exception e) {
@@ -217,7 +139,7 @@ public class GoodReadsUtils {
         return formattedResultsList;
     }
 
-    public String XMLGetTagValue(Element parentElement, String tagName) {
+    private static String getXMLTagValue(Element parentElement, String tagName) {
         NodeList NodeList = parentElement.getElementsByTagName(tagName);
         Element tagElement = (Element)NodeList.item(0);
         Node valueNode = tagElement.getChildNodes().item(0);
